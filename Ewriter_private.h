06fb5b4c9178c598df1732e8d9408b643c200fa8
tcthn <117869197+tcthn@users.noreply.github.com>
2022-11-10 01:10:24 -0600

    Add files via upload

---
+++ Ewriter_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef EWRITER_PRIVATE_H
+#define EWRITER_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.9.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	9
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Edev"
+#define FILE_VERSION	"1.9.0.0"
+#define FILE_DESCRIPTION	"Potente editor de texto"
+#define INTERNAL_NAME	"Ewritter"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Ewritter"
+#define PRODUCT_NAME	"Ewritter"
+#define PRODUCT_VERSION	"1.9.0.0"
+
+#endif /*EWRITER_PRIVATE_H*/
---
+++ main.h
@@ -0,0 +1,17 @@
+#define CM_FILE_TEST          9082
+#define CM_FILE_UPDATE        9081
+#define CM_FILE_SELECTFONT    9080
+#define CM_FILE_FORMAT  	  9079
+#define CM_FILE_COPY          9078
+#define CM_FILE_CUT           9077
+#define CM_FILE_PASTE         9076
+#define CM_FILE_EDIT          9075
+#define CM_FILE_UNDO          9074
+#define CM_FILE_REDO          9073
+#define CM_FILE_SAVEAS	      9072
+#define CM_FILE_EXIT	      9071
+#define CM_FILE_OPEN	      9070
+#define CM_ABOUT              9069
+
+
+#define IDC_MAIN_TEXT 1001
